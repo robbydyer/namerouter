c66f72804b9f48e1083cec9801cf7860d4ec233e
Robby Dyer <rdyer@chegg.com>
2022-12-18 10:48:52 -0500

    wip

--- internal/namerouter/util.go
+++ internal/namerouter/util.go
@@ -4,7 +4,11 @@ func getHosts(nh []*Namehost) []string {
 	hosts := []string{}
 
 	for _, n := range nh {
-		hosts = append(hosts, n.Hosts...)
+		for _, host := range n.Hosts {
+			if host != "default" {
+				hosts = append(hosts, host)
+			}
+		}
 	}
 
 	return hosts
